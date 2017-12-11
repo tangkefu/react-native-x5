37efe7c5a7dcd5b17c68a08542cc4801b9ad653a
tommy <tang.kefu@hotmail.com>
2017-12-11 16:57:55 +0800

    rn 47

--- android/src/main/java/im/shimo/react/x5/webview/RNX5WebViewPackage.java
+++ android/src/main/java/im/shimo/react/x5/webview/RNX5WebViewPackage.java
@@ -42,7 +42,6 @@ public class RNX5WebViewPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
